20a7b1ebbeb7b9ac26ad66f499679bd5eb7164b8
Tsedeke Techane <tsedeketechane@gmail.com>
2025-04-29 20:34:04 +0300

    userController is done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,93 @@
-function register(req, res) {
-    res.send('Register route');
+import dbConnection from "../dbConfig/dbConfig.js"; 
+import bcrypt from 'bcrypt';   
+
+async function register(req, res) {
+    const { username, firstname, lastname, email, password } = req.body;
+    
+    if (!username || !firstname || !lastname || !email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+    try {
+        // Initialize the connection
+        const connection = await dbConnection.getConnection();
+
+        // check if the user already exists
+        const checkUserQuery = "SELECT * FROM users WHERE username = ? OR email = ?";
+        const [rows] = await connection.query(checkUserQuery, [username, email]);
+        if (rows.length > 0) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        // password length validation
+        if (password.length < 8) {
+            return res.status(400).json({ message: "Password must be at least 8 characters long" });
+        }
+
+        // hash the password
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+
+        // email format validation
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+        
+        // Insert the new user into the database
+        const query = "INSERT INTO users (username, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)";
+        const values = [username, firstname, lastname, email, hashedPassword];
+        
+        await connection.query(query, values);
+        res.status(201).json({ message: "User registered successfully" });
+
+
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).json({ message: "Something went wrong. Please try again later!" });
+    }
+
+    
 }
 
-function login(req, res) {
-    res.send('Login route');
+async function login(req, res) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+    try {
+        
+        // Initialize the connection
+        const connection = await dbConnection.getConnection();
+
+        // Check if the user exists
+        const query = "SELECT * FROM users WHERE email = ?";
+        const [rows] = await connection.query(query, [email]);
+
+        if (rows.length === 0) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        // Compare the password with the hashed password in the database
+        const user = rows[0];
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        // Successful login
+        res.status(200).json({ message: "Login successful", user: { id: user.id, username: user.username } });
+        
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({ message: "Something went wrong. Please try again later!" });
+        
+    }
 }
 
 
-function checkUser(req, res) {
+async function checkUser(req, res) {
     res.send('Check user route');
 }
 
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dbConnection from './dbConfig/dbConfig.js';
 
 const app = express();
+// json middleware to extract data from the body
+app.use(express.json());
 const port = 3000;
 
 // user routes middleware file
@@ -17,18 +19,19 @@ app.use("/api/users", userRoutes);
 
 
 
-
+// db connection
 async function connectDB() {
   try {
     await dbConnection.getConnection();
     console.log('Successfully connected to the database.');
   } catch (err) {
-    console.error('Error connecting to the database:', err);
+    console.error('Error connecting to the database:', err.message);
   }
 }
 
 connectDB();
 
+// channel routes middleware
 app.listen(port, (err) => {
   if (err) {
     console.log(err.message);
